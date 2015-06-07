71fbfe90661030556910561a221a7c6f4d33fd46
reo-dstar <ryusei0728@gmail.com>
2015-06-07 18:43:55 +0900

    フロントをnginxでリバースプロキシーする方式に変更しました

--- unicorn.rb
+++ unicorn.rb
@@ -6,7 +6,10 @@ worker_processes 2
 working_directory @dir
 
 timeout 300
-listen 8080
+# listen UNIXsocket
+# listen "/path/to/app/tmp/unicorn.sock", backlog: 1024 # UNIXソケットを見るように変更
+listen "/Users/reo-dstar/work/git/go_library/tmp/unicorn.sock", backlog: 1024 # UNIXソケットを見るように変更
+
 
 # pid
 pid "#{@dir}tmp/pids/unicorn.pid" 
@@ -14,3 +17,4 @@ pid "#{@dir}tmp/pids/unicorn.pid"
 # ログ出力
 stderr_path "#{@dir}log/unicorn.stderr.log"
 stdout_path "#{@dir}log/unicorn.stdout.log"
+
